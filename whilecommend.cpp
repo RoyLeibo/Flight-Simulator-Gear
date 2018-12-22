eed353b9370c383aa1494b3f78a3ec65ce08110a
einatheletz <einatheletz4@gmail.com>
2018-12-22 17:03:46 -0500

    add while commend

--- whilecommend.cpp
+++ whilecommend.cpp
@@ -11,6 +11,7 @@ whilecommend::whilecommend(vector<vector<string>> vec, maps* map)
 }
 void whilecommend:: execute()
 {
+    bool flg = true;
     double variable_one  = 0;
     double variable_two = 0;
     if(s_map->is_value_in_map("symbols_tables",s_vec[0][1]))
@@ -38,7 +39,46 @@ void whilecommend:: execute()
     {
         variable_two = stod(s_vec[0][3]);
     }
+    switch(s_map->get_int("map_operators",s_vec[0][2]))
+    {
+        case 1:
+            if(variable_one < variable_two)
+            {
+                do_while(vec);
+                flg = false;
+            }
+        case 2:
+            if(variable_one > variable_two)
+            {
+                do_while(vec);
+                flg = false;
+            }
+        case 3:
+            if(variable_one == variable_two)
+            {
+                do_while(vec);
+                flg = false;
+            }
+        case 4:
+            if(variable_one != variable_two)
+            {
+                do_while(vec);
+                flg = false;
+            }
+        case 5:
+            if(variable_one >= variable_two)
+            {
+                do_while(vec);
+                flg = false;
+            }
+        case 6:
+            if(variable_one <= variable_two)
+            {
+                do_while(vec);
+                flg = false;
+            }
 
+    }
 
 
 }
