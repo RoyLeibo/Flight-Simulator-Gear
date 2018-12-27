a4377751dab752c2507321d69b7da474224741f5
RoyLeibo <royleibo212@gmail.com>
2018-12-27 20:58:51 +0200

    changes

---
+++ Enterc.cpp
@@ -0,0 +1,10 @@
+//
+// Created by roy on 12/27/18.
+//
+
+#include "Enterc.h"
+
+void Enterc::execute() {
+    char c ;
+    cin >> c ;
+}
---
+++ Enterc.h
@@ -0,0 +1,16 @@
+//
+// Created by roy on 12/27/18.
+//
+
+#ifndef UNTITLED11_ENTERC_H
+#define UNTITLED11_ENTERC_H
+
+#include "command.h"
+
+class Enterc: public command {
+public:
+    void execute() ;
+};
+
+
+#endif //UNTITLED11_ENTERC_H
